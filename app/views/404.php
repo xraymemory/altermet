855cf9b46088fad317ad8d2cb8855f0157a44627
Michael Anzuoni <manzuoni@appnexus.com>
2014-11-06 21:52:52 -0500

    Ludicrously oversized first commit but hey

---
+++ app/views/404.php
@@ -0,0 +1,4 @@
+<h2>Page not found</h2>
+<p class="pagination">
+  <em>What you’re looking for is not here</em>
+</p>
---
+++ app/views/_menu.php
@@ -0,0 +1,19 @@
+<div id="menu" class="hidden">
+  <div class="container">
+    <nav>
+      <ul>
+        <?php
+        
+        foreach ($containers as $container) {
+          $name = htmlentities($container->name,  ENT_COMPAT, 'UTF-8');
+          echo "<li><a href=\"c/$container->id\">$name</a></li>\n";
+        }
+        
+        ?>
+        <li><a href="forum">Forum</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="admin">Admin</a></li>
+      </ul>
+    </nav>
+  </div>
+</div>
